30d6d7adb50c1ab189f5ec0b03162984e1e11607
Umut Çiftci <umutciftci@outlook.com>
2020-02-10 00:23:32 +0300

    Add files via upload

--- content_script.js
+++ content_script.js
@@ -9,3 +9,4 @@ setInterval(function() {
   });
 }, 200);
 
+
--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 function injectTheScript() {
     // Gets all tabs that have the specified properties, or all tabs if no properties are specified (in our case we choose current active tab)
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({'url': 'https://twitch.tv/'}, function(tabs) {
         // Injects JavaScript code into a page
-        chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
+        if(tabs.length > 0) {
+            chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
+        }
+        else {
+            chrome.tabs.create({'url':'https://twitch.tv'});
+        }
+        
     });
 }
 // adding listener to your button in popup window
 document.getElementById('clickactivity').addEventListener('click', injectTheScript);
+
+        
